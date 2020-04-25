580d9ab842efae2934451aaaf7079c6aa569cac3
Marten Truu <marten.truu@tallink.ee>
2020-04-25 20:15:28 +0300

    Move store to Main file

--- SwiftUITEA/Main.swift
+++ SwiftUITEA/Main.swift
@@ -113,5 +113,33 @@ struct RepoRow: View {
     }
 }
 
+// MARK: STORE
 
+func createStore() -> Store<Model, Msg> {
+    Store(initialModel: Model.init(), update: update)
+}
 
+final class Store<Model, Msg>: ObservableObject {
+    @Published private(set) var model: Model
+
+    private let update: (Model, Msg) -> (Model, AnyPublisher<Msg, Never>)
+    private var effectCancellables: Set<AnyCancellable> = []
+
+    init(
+        initialModel: Model,
+        update: @escaping (Model, Msg) -> (Model, AnyPublisher<Msg, Never>)
+    ) {
+        self.model = initialModel
+        self.update = update
+    }
+
+    func send(_ msg: Msg) {
+        let (newModel, effect) = update(model, msg)
+
+        self.model = newModel
+        effect
+            .receive(on: DispatchQueue.main)
+            .sink(receiveValue: send)
+            .store(in: &effectCancellables)
+    }
+}
--- SwiftUITEA/SceneDelegate.swift
+++ SwiftUITEA/SceneDelegate.swift
@@ -3,14 +3,13 @@ import SwiftUI
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     var window: UIWindow?
-    let store = Store<Model, Msg>(initialModel: Model.init(), update: update)
 
     func scene(
         _ scene: UIScene,
         willConnectTo session: UISceneSession,
         options connectionOptions: UIScene.ConnectionOptions
     ) {
-        let contentView = MainView().environmentObject(store)
+        let contentView = MainView().environmentObject(createStore())
 
         if let windowScene = scene as? UIWindowScene {
             let window = UIWindow(windowScene: windowScene)
